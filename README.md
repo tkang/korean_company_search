8af9f7d89104c0bde754e01056b9f15eadb35f34
Taeho Kang <tkang1@gmail.com>
2021-05-07 15:09:15 +0900

    search ui

--- README.md
+++ README.md
@@ -499,3 +499,67 @@ $ ruby bulk_insert_to_dynamodb.rb ~/Downloads/sample_data.json
 ```
 
 콘솔로 들어가 데이터가 잘 들어갔는지 확인해봅시다. Amplicy Console 로 들어가 Backend -> API -> View in AppSync -> Data Sources 에서 확인 가능합니다.
+
+## 검색 창 페이지
+
+검색 쿼리를 입력받는 페이지를 만들어봅시다.
+
+**pages/index.js** 를 다음과 같이 변경합니다.
+
+```js
+function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Korean Company Search</title>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔍</text></svg>"
+        />
+      </Head>
+
+      <div className="container mx-auto">
+        <main>
+          <div className="bg-white">
+            <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+              <div className="text-center">
+                <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+                  Korean Company Search
+                </p>
+                <p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
+                  회사이름, 주소, 사업자등록번호, 우편번호로
+                  <br />
+                  한국에 등록된 회사들을 검색해보세요.
+                </p>
+                <div className="mt-5 mx-auto text-xl w-2/4">
+                  <div className="mt-1 relative rounded-md shadow-sm">
+                    <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                      🔍
+                    </div>
+                    <input
+                      type="text"
+                      name="email"
+                      id="email"
+                      className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-10 sm:text-sm border-gray-300 rounded-md"
+                    />
+                  </div>
+                </div>
+                <div className="mt-5">
+                  <button
+                    type="button"
+                    className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Search Companies
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
+```
--- pages/index.js
+++ pages/index.js
@@ -7,25 +7,49 @@ function Home() {
         <title>Korean Company Search</title>
         <link
           rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔍🏢🏬🔍</text></svg>"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔍</text></svg>"
         />
       </Head>
 
       <div className="container mx-auto">
-        <main className="flex-1 flex-col justify-center items-center">
-          <h1 className="text-6xl">Welcome to Korean Company Search</h1>
-
-          <p className="text-2xl">
-            Let's get started with Korean Company Search
-          </p>
-
-          <div>Hello World</div>
+        <main>
+          <div className="bg-white">
+            <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+              <div className="text-center">
+                <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+                  Korean Company Search
+                </p>
+                <p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
+                  회사이름, 주소, 사업자등록번호, 우편번호로
+                  <br />
+                  한국에 등록된 회사들을 검색해보세요.
+                </p>
+                <div className="mt-5 mx-auto text-xl w-2/4">
+                  <div className="mt-1 relative rounded-md shadow-sm">
+                    <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                      🔍
+                    </div>
+                    <input
+                      type="text"
+                      name="email"
+                      id="email"
+                      className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-10 sm:text-sm border-gray-300 rounded-md"
+                    />
+                  </div>
+                </div>
+                <div className="mt-5">
+                  <button
+                    type="button"
+                    className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Search Companies
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </main>
       </div>
-
-      <footer className="flex items-center justify-center border-t-1 h-8">
-        Footer Here
-      </footer>
     </div>
   );
 }
